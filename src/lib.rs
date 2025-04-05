fb90c33933b4baa51f909fc4939b10fbd8c7a5ca
Simon Jung <simonjung1603@gmail.com>
2025-04-05 22:40:58 +0200

    bump bevy dependencies to 0.15

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,10 @@
 use bevy_app::{App, Plugin};
 use bevy_asset::{
-    io::Reader, Asset, AssetApp, AssetLoader, AsyncReadExt,
-    Handle, LoadContext, LoadedAsset,
+    io::Reader, Asset, AssetApp, AssetLoader, AsyncReadExt, Handle, LoadContext, LoadedAsset,
 };
+use bevy_image::Image;
 use bevy_math::{URect, UVec2};
 use bevy_reflect::TypePath;
-use bevy_render::texture::Image;
 use bevy_sprite::TextureAtlasLayout;
 use thiserror::Error;
 
@@ -20,9 +19,8 @@ pub struct KenneyAssetPlugin;
 
 impl Plugin for KenneyAssetPlugin {
     fn build(&self, app: &mut App) {
-        app
-          .init_asset::<KenneySpriteSheetAsset>()
-          .init_asset_loader::<KenneySpriteSheetAssetLoader>();
+        app.init_asset::<KenneySpriteSheetAsset>()
+            .init_asset_loader::<KenneySpriteSheetAssetLoader>();
     }
 }
 
@@ -68,82 +66,60 @@ impl AssetLoader for KenneySpriteSheetAssetLoader {
     type Asset = KenneySpriteSheetAsset;
     type Settings = ();
     type Error = KenneySpriteSheetAssetLoaderError;
-    async fn load<'a>(
-        &'a self,
-        // TODO: 0.15
-        // reader: &'a mut dyn Reader,
-        reader: &'a mut Reader<'_>,
-        _settings: &'a (),
-        load_context: &'a mut LoadContext<'_>,
+    async fn load(
+        &self,
+        reader: &mut (dyn Reader),
+        _settings: &(),
+        load_context: &mut LoadContext<'_>,
     ) -> Result<Self::Asset, Self::Error> {
         // original path must be the xml file
-        let original_path =
-            load_context.asset_path().path();
-        let image_path =
-            original_path.with_extension("png");
-        let image = load_context
+        let original_path = load_context.asset_path().path();
+        let image_path = original_path.with_extension("png");
+
+        let image: LoadedAsset<Image> = load_context
             .loader()
-            .direct()
+            .immediate()
             .load::<Image>(image_path.clone())
             .await?;
-        let sheet_handle: Handle<Image> =
-            load_context.load(image_path);
-
-        let spritesheet_image = image.get();
-        let spritesheet_size = spritesheet_image.size();
+        let sheet_handle: Handle<Image> = load_context.load(image_path);
+        let spritesheet_size = image.get().size();
 
         let mut xml_string = String::new();
         reader.read_to_string(&mut xml_string).await?;
 
         let doc = roxmltree::Document::parse(&xml_string)?;
 
-        let sheet_dimensions = UVec2::new(
-            spritesheet_size.x,
-            spritesheet_size.y,
-        );
+        let sheet_dimensions = UVec2::new(spritesheet_size.x, spritesheet_size.y);
 
-        let mut layout =
-            TextureAtlasLayout::new_empty(sheet_dimensions);
+        let mut layout = TextureAtlasLayout::new_empty(sheet_dimensions);
         let sub_textures = doc
             .descendants()
-            .filter(|element| {
-                element.tag_name() == "SubTexture".into()
-            })
+            .filter(|element| element.tag_name() == "SubTexture".into())
             .map(|tex| {
                 let x: u32 = tex.attribute("x")?.parse().ok()?;
                 let y: u32 = tex.attribute("y")?.parse().ok()?;
-                let width: u32 =
-                    tex.attribute("width")?.parse().ok()?;
-                let height: u32 =
-                    tex.attribute("height")?.parse().ok()?;
+                let width: u32 = tex.attribute("width")?.parse().ok()?;
+                let height: u32 = tex.attribute("height")?.parse().ok()?;
 
                 layout.add_texture(URect::from_corners(
                     UVec2::new(x, y),
-                    UVec2::new(
-                        x + width,
-                        y + height,
-                    ),
+                    UVec2::new(x + width, y + height),
                 ));
                 Some(SubTexture {
-                    name: tex
-                        .attribute("name")?
-                        .to_string(),
+                    name: tex.attribute("name")?.to_string(),
                     x,
                     y,
                     width,
                     height,
                 })
             })
-            .collect::<Option<Vec<SubTexture>>>().ok_or(
-                KenneySpriteSheetAssetLoaderError::InvalidSubTexture
-            )?;
-        let texture_atlas_layout =
-            LoadedAsset::from(layout);
-        let layout_handle = load_context
-            .add_loaded_labeled_asset(
-                "texture_atlas_layout",
-                texture_atlas_layout,
-            );
+            .collect::<Option<Vec<SubTexture>>>()
+            .ok_or(KenneySpriteSheetAssetLoaderError::InvalidSubTexture)?;
+
+        let texture_atlas_layout = LoadedAsset::from(layout);
+        let layout_handle =
+            load_context.add_loaded_labeled_asset("texture_atlas_layout", texture_atlas_layout);
+
         Ok(KenneySpriteSheetAsset {
             textures: sub_textures,
             sheet: sheet_handle,
